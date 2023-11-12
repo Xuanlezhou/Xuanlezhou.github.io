b2c162844868d8214924f0ca5c4a310ca38b534b
Xuanlezhou <59140401+Xuanlezhou@users.noreply.github.com>
2023-11-12 14:45:26 +0800

    Add files via upload

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,4 +1,3 @@
-import { MdWork, MdAlternateEmail } from "react-icons/md";
 import Image from "next/image";
 import Footer from "../components/footer";
 import Navbar from "../components/navbar";
@@ -7,12 +6,19 @@ export default function Projects() {
   return (
     <main>
       <Navbar />
-      <div className="relative h-96 w-full">
+      <div className="flex relative h-96 w-full justify-center">
         <Image
-          src="/photos/contact.jpg"
+          src="/photos/projects.jpg"
           alt="Logo"
           fill={true}
         />
+        <div className="absolute bg-white bottom-0 py-16 px-32 flex flex-col">
+          <div className="text-4xl pb-8">My Projects</div>
+          <div className="max-w-2xl pt-8">Thanks for stopping by!
+            Here&apos;s a list of projects that I&apos;ve previously worked on.
+            You can tap on them to find out more 😊
+          </div>
+        </div>
       </div>
       <Footer />
     </main>
