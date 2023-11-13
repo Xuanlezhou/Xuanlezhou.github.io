a8023ad37f7548b53507d72d838df9f41079b065
Xuanlezhou <xuanlezhou@gmail.com>
2023-11-13 12:41:40 +0000

    Add all pages

--- src/app/projects/juicer/page.tsx
+++ src/app/projects/juicer/page.tsx
@@ -4,9 +4,9 @@ import Carousel from "@/app/components/carousel";
 import ProjectDetail from "@/app/components/project-detail";
 
 export default function Juicer() {
-  const title = "Tele-Operated Robotic Arm"
+  const title = "Sugar Cane Juicer"
   const location = "Singapore"
-  const dateRange = "May 2021 – Aug 2021"
+  const dateRange = "Jan 2022 – May 2022"
   const content = "A Tele-Operated Robotic Arm with a 6DOF controller that allows the user to move freely within a space while detecting in real time, it’s exact position to control our 6 DOF robotic output arm. For the technically curious, 6 encoders in the controller will detect the movement by measuring the joint angles, which is then translated into a pose in 3D space using forward kinematics. Our integration system will then use inverse kinematics to calculate the joint angles for the 6 servomotors of the output robotic arm."
   const supplement = "This project was completed under the 'Making and Tinkering' program in NTU where we received a funding of $2000 and emerged 1st amongst 24 other groups."
   return (
--- src/app/projects/sunfryer/page.tsx
+++ src/app/projects/sunfryer/page.tsx
@@ -4,9 +4,9 @@ import Carousel from "@/app/components/carousel";
 import ProjectDetail from "@/app/components/project-detail";
 
 export default function Sunfryer() {
-  const title = "Tele-Operated Robotic Arm"
-  const location = "Singapore"
-  const dateRange = "May 2021 – Aug 2021"
+  const title = "Sunfryer"
+  const location = "Berkeley, CA"
+  const dateRange = "Jun 2022 – July 2022"
   const content = "A Tele-Operated Robotic Arm with a 6DOF controller that allows the user to move freely within a space while detecting in real time, it’s exact position to control our 6 DOF robotic output arm. For the technically curious, 6 encoders in the controller will detect the movement by measuring the joint angles, which is then translated into a pose in 3D space using forward kinematics. Our integration system will then use inverse kinematics to calculate the joint angles for the 6 servomotors of the output robotic arm."
   const supplement = "This project was completed under the 'Making and Tinkering' program in NTU where we received a funding of $2000 and emerged 1st amongst 24 other groups."
   return (
